743c64f919c3746fe822220efec69cd7b796a312
weixx <1013063371@qq.com>
2022-02-06 02:31:34 +0800

    second commit

--- src/a/b/c/GitTest.java
+++ src/a/b/c/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("测试Git");
         System.out.println("第二次测试Git");
+        System.out.println("第三次测试Git");
     }
 }
